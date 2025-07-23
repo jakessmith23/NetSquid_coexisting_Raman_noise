aabc6c032db9da06f406ca860f6cda31e343968f
jakessmith23 <39771302+jakessmith23@users.noreply.github.com>
2025-07-23 17:11:25 +0100

    Update characterized_coex_sim.py

--- characterized_coex_sim.py
+++ characterized_coex_sim.py
@@ -46,7 +46,7 @@ def calc_raman_photons(P_launch, rho, alpha_np, wavelengths, kpol=2):
                 ram_photons_per_det_window[wl][l].append(photon_count)
     return ram_photons_per_det_window
 
-def simulate(ram_photons_per_det_window, tele_hardware_params):
+def simulate(ram_photons_per_det_window, tele_hardware_params, fiber_lengths, wavelengths):
     fidelities = {wl: {L: [] for L in fiber_lengths} for wl in wavelengths}
     for wl in wavelengths:
         for l in fiber_lengths:
@@ -68,11 +68,6 @@ def simulate(ram_photons_per_det_window, tele_hardware_params):
 if __name__ == "__main__":
     # Load data
     data = read_measurement_data('RAMAN_Charact.xlsx', 'Meas_1565_HP_DP')
-
-    # CONFIGURABLE: select your lengths and wavelengths of choice
-    fiber_lengths = [10, 20, 40]  # km
-    wavelengths = [1510, 1554, 1563.4, 1566.6]
-
     # Use max input power to estimate rho
     max_power = np.max(data['P_TX'])
     rho = calculate_rho(
@@ -83,8 +78,17 @@ if __name__ == "__main__":
     )
 
     # Raman photon calculation
+     # CONFIGURABLE: select your lengths and wavelengths of choice
+    fiber_lengths = [10, 20, 40]  # km
+    wavelengths = [1510, 1554, 1563.4, 1566.6]
+
     launch_powers_mW = np.linspace(0, 10, 100)
     raman_photons = calc_raman_photons(launch_powers_mW, rho, data['alpha_np'], wavelengths)
+
+    for wl in wavelengths:
+        for l in fiber_lengths:
+            # to access raman photon counts for a certain wavelenth at a certain length 
+            print(raman_photons[wl][l])
 
     # CONFIGURABLE: Select which hardware parameters to use to calculate visibility --> fidelity based on your experiment type
     from entangled_hardware_config import tele_hardware_params  
@@ -92,7 +96,7 @@ if __name__ == "__main__":
     
     # Fidelity simulation
     # CONFIGURABLE: pass the correct hardware_params for your type of experiment
-    fidelities = simulate(raman_photons, tele_hardware_params)
+    fidelities = simulate(raman_photons, tele_hardware_params, fiber_lengths, wavelengths)
 
     colors = ['blue', 'orange', 'green', 'red']
     wavelength_labels = ['1510 nm', '1554 nm', '1563.4 nm', '1566.6 nm']
